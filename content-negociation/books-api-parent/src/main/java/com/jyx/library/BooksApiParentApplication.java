e326e458f19c58914200d8df15ca4c325c1bf253
Wajih <mohamedwajih.elouaer@gmail.com>
2026-09-01 21:25:50 +0100

    implementing content negociation: in progress

---
+++ content-negociation/books-api-parent/src/main/java/com/jyx/library/BooksApiParentApplication.java
@@ -0,0 +1,13 @@
+package com.jyx.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksApiParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksApiParentApplication.class, args);
+	}
+
+}
---
+++ content-negociation/books-api-parent/src/test/java/com/jyx/library/BooksApiParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyx.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksApiParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
